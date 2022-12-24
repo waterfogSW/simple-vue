4dda6836d918e613ab35eb09dfe94df19b2fd7a3
waterfog9580 <waterfog9580@gmail.com>
2022-12-24 17:32:16 +0900

    :tada: vue 학습용 프로젝트 구성

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Header from './components/Header.vue'
+import Menu from './components/Menu.vue'
+import Content from './components/Content.vue';
+
+export default {
+  name: "app",
+  components: { // 가져온 component 들을 등록합니다.
+    Header,
+    Menu,
+    Content
+  }
+};
+</script>
+
+<template>
+  <Header/>
+  <div class="wrap">
+    <Menu/>
+    <Content/>
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  display: flex;
+}
+</style>
