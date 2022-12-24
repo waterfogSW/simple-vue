e3bd8e4948e86dd308eb894d0d7de357ce52ab8d
waterfog9580 <waterfog9580@gmail.com>
2022-12-24 22:41:49 +0900

    :sparkles: v-for를 통한 리스트 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,47 @@
 <script>
-import Header from './components/Header.vue'
-import Menu from './components/Menu.vue'
-import Content from './components/Content.vue';
-import Box from './components/Box.vue'
+import Webtoon from "@/components/Webtoon.vue";
 
 export default {
   name: "app",
-  components: { // 가져온 component 들을 등록합니다.
-    Header,
-    Menu,
-    Content,
-    Box
+  components: {Webtoon},
+  data() {
+    return {
+      webtoons: [
+        {
+          name: "햄스터와 그녀",
+          link: "http://webtoon.daum.net/webtoon/view/hamsterandher",
+          img:
+              "http://t1.daumcdn.net/webtoon/op/478cdf37f585607982ffa9e35b432e8503be8a54"
+        },
+        {
+          name: "프롬 스타",
+          link: "http://webtoon.daum.net/webtoon/view/fromstar",
+          img:
+              "http://t1.daumcdn.net/webtoon/op/a7fb953d722c1130bfc18440f7e3ce448ece57a1"
+        },
+        {
+          name: "위대한 로맨스",
+          link: "http://webtoon.daum.net/webtoon/view/greatromance",
+          img:
+              "http://t1.daumcdn.net/webtoon/op/a816281cb4df5c50a20ac386fd6e496643d0f085"
+        },
+        {
+          name: "빛나는 손을",
+          link: "http://webtoon.daum.net/webtoon/view/Hand",
+          img: "http://t1.daumcdn.net/cartoon/5913FCAC0234C50001"
+        }
+      ]
+    };
   }
 };
+
 </script>
 
 <template>
-  <div>
-    <Box color="blue"/>
-    <Box color="purple"/>
-    <Box color="green"/>
-    <Box color="blue"/>
-    <Box color="purple"/>
-    <Box />
+  <div id="app">
+    <Webtoon :items="webtoons"/>
   </div>
 </template>
 
 <style scoped>
-.wrap {
-  display: flex;
-}
 </style>
---
+++ src/components/Webtoon.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h2>Webtoon</h2>
+    <ul class="wrap">
+      <li class="item" v-for="(item, idx) in items" :key="{idx}">
+        <a :href="item.link" target="_blank">
+          <img :src="item.img" alt="이미지가 존재하지 않습니다"/>
+          <span class="title">{{item.name}}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Webtoon",
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+// Webtoon.vue
+
+<style scoped>
+h2 {
+  text-align: center;
+}
+
+a {
+  list-style: none;
+  text-decoration: none;
+}
+
+li {
+  list-style: none;
+}
+
+.wrap {
+  max-width: 450px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.item {
+  border-bottom: 1px solid #ebebeb;
+  margin-bottom: 25px;
+}
+
+.title {
+  display: inline-block;
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
+  padding: 20px 15px;
+}
+
+img {
+  width: 100%;
+  background: #ebebeb;
+  border-radius: 4px;
+}
+</style>
