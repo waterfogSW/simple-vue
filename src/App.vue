caedb77664ca700dd880527c3d8a520c4951b44a
waterfog9580 <waterfog9580@gmail.com>
2022-12-24 21:01:39 +0900

    :sparkles: state 와 prop

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,27 @@
 import Header from './components/Header.vue'
 import Menu from './components/Menu.vue'
 import Content from './components/Content.vue';
+import Box from './components/Box.vue'
 
 export default {
   name: "app",
   components: { // 가져온 component 들을 등록합니다.
     Header,
     Menu,
-    Content
+    Content,
+    Box
   }
 };
 </script>
 
 <template>
-  <Header/>
-  <div class="wrap">
-    <Menu/>
-    <Content/>
+  <div>
+    <Box color="blue"/>
+    <Box color="purple"/>
+    <Box color="green"/>
+    <Box color="blue"/>
+    <Box color="purple"/>
+    <Box />
   </div>
 </template>
 
---
+++ src/components/Box.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    :class="['box', color]"
+    :style="{ width: width + 'px', height: height + 'px'}"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    color: {type: String, default: ""}
+  },
+  data() {
+    return {
+      width: 40,
+      height: 80,
+      box: "box"
+    };
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  border: 1px solid #000;
+}
+
+.blue {
+  background: #009bff;
+}
+
+.purple {
+  background: #8f46ff;
+}
+
+.green {
+  background: #00bcac;
+}
+</style>
